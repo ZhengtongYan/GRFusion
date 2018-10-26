e3f1873018a62e13c4e2c5b13c039e2ba2e70599
Spencer Pearson <pearso29@purdue.edu>
2018-10-25 22:23:46 -0700

    Getting data structures together

--- src/ee/graph/EDPIndex.h
+++ src/ee/graph/EDPIndex.h
@@ -2,34 +2,125 @@
 #define GRAPHVIEW_H
 
 #include "GraphView.h"
+#include <unordered_map>
 
 using namespace std;
 
 namespace voltdb {
 
+class Partition;
+
+class PartitionEdge {
+public:
+	PartitionEdge(void);
+	~PartitionEdge(void);
+
+	Edge* getEdge();
+
+	int getID();
+	int getFromID();
+	int getToID();
+
+	float getWeight();
+	int getLabel();
+	bool isDirected();
+
+private:
+	Edge* edge;
+
+	int eID;
+	int fromID;
+	int toID;
+
+	float weight;
+	int label;
+	bool directed;
+};
+
+class PartitionVertex {
+public:
+	PartitionVertex(void);
+	~PartitionVertex(void);
+
+	Vertex* getVertex();
+
+	int getID();
+	bool isBridge();
+	int getComponentID();
+
+	vector<PartitionEdge*>* getInEdges();
+	vector<PartitionEdge*>* getOutEdges();
+
+	vector<Partition*>* getOtherHosts();
+
+	//Adds an edge to this vertex and updates OtherHostsList if applicable
+	void addEdge(PartitionEdge* edge);
+
+private:
+	Vertex* vertex;
+
+	int vID;
+	bool bridge;
+	int componentID;
+
+	vector<PartitionEdge*> inEdges;
+	vector<PartitionEdge*> outEdges;
+
+	vector<Partition*> otherHosts;
+};
+
 class MonochromePath{
-	friend class Vertex;
 
 public:
-	MonochromePath(int src, int dest, int weight)
+	MonochromePath(int src, int dest, int weight);
 protected:
 	int src_id;
 	int dest_id;
 	int weight;
 
+	vector<PartitionEdge*> pathEdges;
+};
+
+class PathCacheEntry {
+public:
+	PathCacheEntry(int src, int dest, int cost, vector<PartitionEdge*>* pathEdges);
+	~PathCacheEntry();
+
+	int getSource();
+	int getDestination();
+	int getCost();
+
+	vector<PartitionEdge*>* getPath();
+
+	//Returns the size of the cache entry in bytes
+	int getEntrySize();
+
+private:
+	int src;
+	int dest;
+	int cost;
+
+	vector<PartitionEdge*> pathEdges;
 };
 
 class Partition{
-	friend class Vertex;
-	friend class Edge;
 
 public:
+	Partition(int label);
+	~Partition();
 
-protected:
-	int label;
-	std::map<int, Vertex* > m_vertexes;
-	std::map<int, Edge* > m_edges;
+	int getLabel();
+
+	vector<map<int, PartitionVertex>>* getPartitions();
+	map<int, PartitionEdge>* getEdges();
+
+	PartitionVertex* addVertex(int id);
+	PartitionEdge* addEdge(int id, int from, int to, float weight, int label, bool addVertexIfNotFound);
 private:
+	int label;
+	//Map of VertexID to PartitionVertex object in this Partition
+	map<int, PartitionVertex > p_vertexes;
+	map<int, PartitionEdge > m_edges;
 };
 
 class EDPIndex{
@@ -45,3 +136,5 @@ private:
 };
 
 }
+
+#endif
