e077c30e4513b58fe8872a07107e0989ce0122f2
Spencer Pearson <pearso29@purdue.edu>
2018-12-07 05:40:02 -0800

    Progress on implementation of EDP index

--- src/ee/graph/EDPIndex.cpp
+++ src/ee/graph/EDPIndex.cpp
@@ -1,6 +1,8 @@
 #include "EDPIndex.h"
 #include "Edge.h"
+#include "Vertex.h"
 #include "common/ValuePeeker.hpp"
+#include <algorithm>
 
 
 using namespace std;
@@ -10,7 +12,7 @@ namespace voltdb
 
 	//PartitionEdge member definitions
 
-	PartitionEdge::PartitionEdge(Edge* edge, PartitionVertex* fromVertex, PartitionVertex* toVertex, int weightField, int labelField, bool directed)
+	PartitionEdge::PartitionEdge(Edge* edge, PartitionVertex* fromVertex, PartitionVertex* toVertex, int weightField, int labelField)
 	{
 		this->edge = edge;
 
@@ -23,14 +25,6 @@ namespace voltdb
 		label = ValuePeeker::peekInteger(edgeTuple.getNValue(labelField));
 	}
 
-	Edge * PartitionEdge::getEdge() { return edge; }
-
-	int PartitionEdge::getID() { return eID; }
-	PartitionVertex* PartitionEdge::getFrom() { return fromV; }
-	PartitionVertex* PartitionEdge::getTo() { return toV; }
-	int PartitionEdge::getLabel() { return label; }
-	double PartitionEdge::getWeight() { return weight; }
-
 	//PartitionVertex member definitions
 
 	PartitionVertex::PartitionVertex(Vertex* vertex)
@@ -38,43 +32,30 @@ namespace voltdb
 		this->vertex = vertex;
 		vID = vertex->getId();
 
-		componentID = -1;
+		componentId = -1;
 		bridge = false;
+		bridgeBackwards = false;
 		partition = NULL;
 	}
 
-	Vertex * PartitionVertex::getVertex() { return vertex; }
-
-	int PartitionVertex::getID() { return vID; }
-	bool PartitionVertex::isBridge() { return bridge; }
-	int PartitionVertex::getComponentID() { return componentID; }
-	Partition* PartitionVertex::getPartition() { return partition; }
-
-	map<int,PartitionEdge*>* PartitionVertex::getInEdges() { return inEdges; }
-	map<int,PartitionEdge*>* PartitionVertex::getOutEdges() { return outEdges; }
-
-	vector<Partition*>* PartitionVertex::getOtherHosts() { return otherHosts; }
-
-	void PartitionVertex::addEdge(PartitionEdge * edge, EDPIndex* edp)
+	void PartitionVertex::addEdge(PartitionEdge * edge, bool outEdge)
 	{
 		assert(partition != NULL);
-		assert(edge->getFrom() == this);
+		assert((outEdge && edge->getFrom() == this) || (!outEdge && edge->getTo() == this));
 
-		outEdges.insert(std::pair<int,PartitionEdge*>(edge->getID(),edge));
-
-		//Insert into in edges in To if not already exists
-		edge->getTo()->getInEdges()->insert(std::pair<int,PartitionEdge*>(edge->getID(),edge));
-
-		//Check if the addition of this edge has added to this vertex's OtherHosts list
-		otherHosts.insert(std::pair<int,Partition*>(edge->getLabel(),edp->getPartition(edge->getLabel())));
-		bridge = otherHosts.empty();
+		if(outEdge)
+			outEdges.insert(std::pair<int,PartitionEdge*>(edge->getID(),edge));
+		else
+			inEdges.insert(std::pair<int,PartitionEdge*>(edge->getID(),edge));
 	}
 
 	//Partition member definitions
 
-	Partition::Partition(int label)
+	Partition::Partition(EDPIndex* index, int label)
 	{
+		this->index = index;
 		this->label = label;
+		this->numOfComponents = 0;
 	}
 
 	Partition::~Partition()
@@ -83,9 +64,234 @@ namespace voltdb
 		delete p_edges;
 	}
 
-	int Partition::getLabel() { return label; }
-	map<int, PartitionVertex>* Partition::getVertexes() { return p_vertexes; }
-	map<int, PartitionEdge>* Partition::getEdges() { return p_edges; }
+
+	PartitionVertex* Partition::addVertex(int vId)
+	{
+		PartitionVertex* pv = new PartitionVertex(index->graph->getVertex(vId));
+		p_vertexes[vId] = pv;
+		return pv;
+	}
+
+	PartitionVertex* Partition::getVertex(int vId)
+	{
+		PartitionVertex *pv;
+		try
+		{
+			pv = &(getVertexes()->at(vId));
+		} catch(const std::out_of_range& oor){}
+		return pv;
+	}
+
+	PartitionEdge* Partition::addEdge(int id, int from, int to, int weightField, int labelField)
+	{
+		PartitionEdge* pe = new PartitionEdge(index->graph->getEdge(id), index->graph->getVertex(from), index->graph->getVertex(to),
+				weightField, labelField);
+
+		p_edges[id] = pe;
+		return pe;
+	}
+
+	PartitionEdge* Partition::getEdge(int eId)
+	{
+		PartitionEdge *pe;
+		try
+		{
+			pe = &(getEdges()->at(eId));
+		} catch(const std::out_of_range& oor){}
+		return pe;
+	}
+
+	float Partition::getEdgeWeight(int from, int to)
+	{
+		//Probe cache for
+		PathCacheEntry *pce = NULL;
+		try
+		{
+			pce = &(index->cache.at(label).at(from).at(to));
+		} catch(const std::out_of_range& oor){}
+		if(pce == NULL || (pce != NULL && pce->timestamp < connectedComponents.getLastTimestamp()))
+		{
+
+		}
+	}
+
+	void Partition::buildCC()
+	{
+		int numVertexes = p_vertexes.size();
+		vector<bool>isVisited = vector<bool>(numVertexes, false);
+
+		map<int, int> globalToLocalId;
+		int nextLocalId = 0;
+
+		numOfComponents = 0;
+		int currentComponentId = 0;
+
+		for(map<int, PartitionVertex>::iterator it = p_vertexes.begin(); it != p_vertexes.end(); it++)
+		{
+			it->second.setComponentId(-1);
+			globalToLocalId.insert(it->first, nextLocalId++);
+		}
+		for(map<int, PartitionVertex>::iterator it = p_vertexes.begin(); it != p_vertexes.end(); it++)
+		{
+			CC_dfs(&(it->second), isVisited, &globalToLocalId, currentComponentId);
+		}
+		connectedComponents = PartitionConnectedComponents(numOfComponents, this);
+	}
+
+	void Partition::CC_dfs(PartitionVertex* startVertex, vector<bool>& isVisited, map<int, int>* gToLId, int& curCompId)
+	{
+		vector<PartitionVertex*> stack;
+		stack.push_back(startVertex);
+		PartitionVertex *currentVertex = NULL, *reachableVertex = NULL;
+
+		while(!stack.empty())
+		{
+			currentVertex = stack.pop_back();
+			currentVertex->componentId = curCompId;
+			isVisited[gToLId[currentVertex->getID()]] = true;
+			//Iterate over out edges
+			for(map<int,PartitionEdge*>::iterator out = currentVertex->outEdges.begin(); out != currentVertex->outEdges.end(); out++)
+			{
+				PartitionVertex* rVertex = out->second->toV;
+				if(!isVisited[gToLId[rVertex->getID()]])
+				{
+					stack.push_back(rVertex);
+				}
+			}
+			//Iterate over in edges
+			for(map<int,PartitionEdge*>::iterator in = currentVertex->inEdges.begin(); in != currentVertex->outEdges.end(); in++)
+			{
+				PartitionVertex* rVertex = in->second->fromV;
+				if(!isVisited[gToLId[rVertex->getID()]])
+				{
+					stack.push_back(rVertex);
+				}
+			}
+		}
+		numOfComponents = ++curCompId;
+	}
+
+	double Partition::dijkstra(PartitionVertex* startVertex, PartitionVertex* endVertex)
+	{
+		float distance = -1;
+		DistFromSource* dist = new DistFromSource(startVertex->vID, 0), *toDist;
+		map<int, DistFromSource*> distMap = new map<int, DistFromSource>;
+		distMap[startVertex->vID] = *dist;
+		distfromsourcepq_type pq;
+
+		PartitionVertex* u, v;
+
+		pq.push(dist);
+		while(!pq.empty())
+		{
+			dist = pq.pop();
+			u = getVertex(dist->vertexId);
+			//Explore only direct monoedges
+			for(auto e = u->outEdges.begin(); e != u->outEdges.end(); e++)
+			{
+				v = e->second->toV;
+				toDist = NULL;
+				try
+				{
+					toDist = distMap.at(v.vID);
+				} catch(const std::out_of_range& oor){}
+
+				if(toDist == NULL)
+				{
+					toDist = new DistFromSource(v.vID, dist->distance + e->second->weight);
+					pq.push(toDist);
+					distMap[v.vID] = toDist;
+				}
+				else if(toDist->distance > dist->distance + e->second->weight)
+				{
+					toDist->distance = dist->distance + e->second->weight;
+				}
+			}
+		}
+
+		delete distMap;
+
+	}
+
+	DistFromSource::DistFromSource(int vId, double dist)
+	{
+		vertexId = vId;
+		distance = dist;
+	}
+
+	//PathCacheEntry member definitions
+
+	PathCacheEntry::PathCacheEntry(int label, int src, int dest, int cost, vector<PartitionEdge*>* pathEdges)
+	{
+		this->label = label;
+		this->src = src;
+		this->dest = dest;
+		this->cost = cost;
+		this->pathEdges = *(new vector<PartitionEdge*>);
+		lru_access = 0;
+		this->timestamp = INT_MAX;
+		for(vector<PartitionEdge*>::iterator peI=pathEdges->begin(); peI!=pathEdges->end(); peI++)
+		{
+			this->pathEdges.push_back(*peI);
+		}
+	}
+
+	uint64_t PathCacheEntry::getEntrySize()
+	{
+		return (sizeof(int) * 5) + sizeof(vector<PartitionEdge*>*)+ sizeof(PartitionEdge*) * pathEdges.size();
+	}
+
+	//PartitionConnectedComponents member definitions
+
+	PartitionConnectedComponents::PartitionConnectedComponents(int initialNumOfComponents, Partition* partition)
+	{
+		numOfConnectedComponents = initialNumOfComponents;
+		for(int i = 0; i < numOfConnectedComponents; i++)
+		{
+			componentLastUpdateTimestamp.insert(i,0);
+		}
+		lastTimestamp = 0;
+		this->partition = partition;
+	}
+
+	void PartitionConnectedComponents::addConnectedComponent(int componentId)
+	{
+		componentLastUpdateTimestamp.insert(componentId, getLastTimestamp()+1);
+		advanceTimestamp();
+	}
+
+	void PartitionConnectedComponents::removeConnectedComponent(int componentId)
+	{
+		map<int,int>::iterator it = componentLastUpdateTimestamp.find(componentId);
+		if(it != componentLastUpdateTimestamp.end())
+			componentLastUpdateTimestamp.erase(it);
+	}
+
+	void PartitionConnectedComponents::advanceComponentTimestamp(int componentId)
+	{
+		componentLastUpdateTimestamp.at(componentId) = advanceTimestamp();
+	}
+
+	void PartitionConnectedComponents::mergeConnectedComponents(int componentId1, int componentId2)
+	{
+		map<int,int>::iterator it1, it2;
+		it1 = componentLastUpdateTimestamp.find(componentId1);
+		it2 = componentLastUpdateTimestamp.find(componentId2);
+		assert(it1 != componentLastUpdateTimestamp.end() && it2 != componentLastUpdateTimestamp.end());
+
+		componentLastUpdateTimestamp.erase(it2);
+		it1->second = advanceTimestamp();
+
+		vector<PartitionVertex*>* cc2_vertexes = partition->cc_vertexes[componentId2];
+
+		//Move vertexes from CC2 to CC1
+		for(vector<PartitionVertex*>::iterator it = cc2_vertexes->begin(); it != partition->cc_vertexes.end(); it++)
+		{
+			(*it)->setComponentId(componentId1);
+		}
+
+		partition->cc_vertexes.erase(componentId2);
+	}
 
 	//EDPIndex member definitions
 
@@ -93,16 +299,246 @@ namespace voltdb
 	{
 		graph = NULL;
 		partitions = NULL;
+		maxCacheSize = 0;
 		cacheSize = 0;
+		cache = NULL;
+		labelColumn = -1;
+		costColumn = -1;
+		lru_clock = 0;
 	}
 
-	static EDPIndex EDPIndex::buildIndex(GraphView* g, int numLabels, int labelColumn, uint32_t cacheSize)
+	EDPIndex::~EDPIndex()
 	{
-		this->cacheSize = cacheSize;
-		graph = g;
-		partitions = new Partition[numLabels];
+		delete partitions;
+		delete cache;
+
+	}
+
+	PartitionEdge* EDPIndex::addEdge(Edge* e)
+	{
+		int edgeLabel = ValuePeeker::peekInteger(graph->getEdgeTuple(e->getId())->getNValue(labelColumn));
+		int edgeCost = ValuePeeker::peekDouble(graph->getEdgeTuple(e->getId())->getNValue(costColumn));
+		Partition * p = &(partitions[edgeLabel]);
+		Vertex *vFrom = e->getStartVertex(), *vTo = e->getEndVertex();
+		PartitionEdge* pEdge = p->addEdge(e->getId(), e->getStartVertexId(), e->getEndVertexId(), edgeCost, edgeLabel);
+		//Check if vertices already exist in partition
+		PartitionVertex *pvFrom = p->getVertex(vFrom->getId()), *pvTo = p->getVertex(vTo->getId());
+
+		//If vertices don't exist in partition yet, add them
+		if(pvFrom == NULL)
+			pvFrom = addVertex(p, vFrom);
+		if(pvTo == NULL)
+			pvTo = addVertex(p, vTo);
+
+		//Add edge to vertices
+		pvFrom->addEdge(pEdge, true);
+		pvTo->addEdge(pEdge, false);
+
+		return pEdge;
+	}
+
+	PartitionVertex* EDPIndex::addVertex(Partition* p, Vertex* v)
+	{
+		PartitionVertex *pvFrom = p->addVertex(v->getId());
+		//Build OtherHosts list
+		map<int, Partition*>* otherHostsOut = pvFrom->getOtherHostsOut();
+		map<int, Partition*>* otherHostsIn = pvFrom->getOtherHostsIn();
+		//Build forwards OtherHosts list with out edges
+		int fanOut = v->fanOut();
+		Edge *outEdge;
+		for(int i = 0; i < fanOut; i++)
+		{
+			int outEdgeId = v->getOutEdgeId(i);
+			int outEdgeLabel = ValuePeeker::peekInteger(graph->getEdgeTuple(outEdgeId)->getNValue(labelColumn));
+			otherHostsOut->insert(std::pair<int, Partition*>(outEdgeLabel, &(partitions[outEdgeLabel])));
+		}
+		//Build backwards OtherHosts list with in edges
+		int fanIn = v->fanIn();
+		Edge *inEdge;
+		for(int i = 0; i < fanOut; i++)
+		{
+			int inEdgeId = v->getInEdgeId(i);
+			int inEdgeLabel = ValuePeeker::peekInteger(graph->getEdgeTuple(inEdgeId)->getNValue(labelColumn));
+			otherHostsIn->insert(std::pair<int, Partition*>(inEdgeLabel, &(partitions[inEdgeLabel])));
+		}
+		//Set bridge flags for forwards/backwards
+		pvFrom->setBridge(!otherHostsOut->empty());
+		pvFrom->setBridgeBackwards(!otherHostsIn->empty());
+
+		return pvFrom;
+	}
+
+	static EDPIndex EDPIndex::buildIndex(GraphView* g, int numLabels, int labelColumn, int costColumn, uint32_t cacheSize)
+	{
+		EDPIndex* edp = new EDPIndex();
+		edp->maxCacheSize = cacheSize;
+		edp->labelColumn = labelColumn;
+		edp->costColumn = costColumn;
+		edp->graph = g;
+		edp->partitions = new vector<Partition>(numLabels, NULL);
+		for(int i = 0; i < numLabels; i++)
+		{
+			edp->partitions[i] = new Partition(edp, i);
+		}
+		edp->cache = new vector<map<int, map<int, PathCacheEntry>>>(numLabels, new map<int, map<int, PathCacheEntry>>);
+		std::map<int, Edge*>* edges = &(g->m_edges);
+		std::map<int, Vertex*>* vertices = &(g->m_vertexes);
+		Partition p;
+
+		bool isBridge;
+
+		for(std::map<int, Edge*>::iterator eIt=edges->begin(); eIt!=edges->end(); eIt++)
+		{
+			edp->addEdge(eIt->second);
+		}
+
+		return edp;
+	}
+
+	void EDPIndex::insertPath(int label, int src, int dest, double cost, vector<PartitionEdge*>* pathEdges)
+	{
+		PathCacheEntry pce = new PathCacheEntry(label, src, dest, cost, pathEdges);
+		uint64_t pce_size = pce.getEntrySize();
+		if(maxCacheSize - cacheSize >= pce_size)
+		{
+			//We have enough space in cache
+			cacheSize+=pce_size;
+			cache[label][src][dest] = pce;
+			lru.push(&pce);
+		}
+		else
+		{
+			PathCacheEntry* lru_pce;
+			uint64_t lru_pce_size;
+			//Remove cache entries by LRU while there's not enough space in the cache
+			while(maxCacheSize - cacheSize < pce_size)
+			{
+				lru_pce = lru.pop();
+				int llabel = lru_pce->getLabel(), lsrc = lru_pce->getSource(), ldest = lru_pce->getDestination();
+				lru_pce_size = lru_pce->getEntrySize();
+				cacheSize -= lru_pce_size;
+				delete cache[llabel][lsrc][ldest];
+				cache[llabel][lsrc].erase(ldest);
+				if(cache[llabel][lsrc].empty())
+					cache[llabel].erase(lsrc);
+			}
+		}
+
+		cache[label][src][dest].setLRU(lru_clock++);
+	}
+
+	PathCacheEntry* EDPIndex::getPath(int label, int src, int dest)
+	{
+		PathCacheEntry* pce = NULL;
+		try
+		{
+			pce = cache.at(label).at(src).at(dest);
+			pce->setLRU(lru_clock++);
+		}catch(const std::out_of_range& oor){}
+		return pce;
+	}
+
+	PartitionVertex* EDPIndex::findVertex(int v, vector<int>* labels)
+	{
+		PartitionVertex* pv = NULL;
+		for(vector<int>::iterator lI=labels->begin(); lI!=labels->end();lI++)
+		{
+			pv = partitions[(*lI)].getVertex(v);
+			if(pv != NULL)
+			{
+				break;
+			}
+		}
+		return pv;
+	}
+
+	//EDPIndex Query Functions
+
+	void EDPIndex::SP_TopK(int src, int dest, int k, vector<int>* labels)
+	{
+		double minCost = DBL_MAX;
+		int foundPathsSoFar = 0;
+		//PQEntryWithLength.first is the cost, PQEntryWithLength.second.first is the vertexId, PQEntryWithLength.second.second is the path length
+		priority_queue<PQEntryWithLength, vector<PQEntryWithLength>, std::greater<PQEntryWithLength>> pq;
+		Partition* currentPartition = NULL;
+		PartitionVertex *pv_src = findVertex(src, labels, *currentPartition), *pv_dest;
+		if(pv_src != NULL && pv_dest != NULL)
+			pq.push(make_pair(0, make_pair(src, 0))); //zero cost to reach src from itself
+		PartitionVertex* pv = NULL;
+		PartitionEdge* pe = NULL;
+		int currVId, fanOut = -1, candVertexId = -1;
+
+		//upper-bound to avoid loops (considering an average fan-out of 10
+		int maxPQOperations = graph->numOfVertexes() * 10;
+		int iterationNum = 0;
+		TableTuple* edgeTuple;
+		int length;
+
+		while(!pq.empty())
+		{
+			//select next vertex to explore
+			currVId = ((pair<int, int >)( pq.top().second)).first;
+			length = ((pair<int, int >)( pq.top().second)).second;
+			minCost = pq.top().first;
+			if(currVId == dest)
+			{
+				foundPathsSoFar++;
+
+				//add a tuple here
+				TableTuple temp_tuple = graph->m_pathTable->tempTuple();
+				//start vertex, end vertex, length, cost, path
+				temp_tuple.setNValue(0, ValueFactory::getIntegerValue(src));
+				temp_tuple.setNValue(1, ValueFactory::getIntegerValue(dest));
+				temp_tuple.setNValue(2, ValueFactory::getIntegerValue(length));
+				temp_tuple.setNValue(3, ValueFactory::getDoubleValue(minCost));
+				//temp_tuple.setNValue(4, ValueFactory::getStringValue("Test", NULL) );
+				if(graph->m_pathTable->activeTupleCount() <= 1000)
+				graph->m_pathTable->insertTempTuple(temp_tuple);
+			}
+			iterationNum++;
+
+			if(foundPathsSoFar == k || iterationNum == maxPQOperations)
+			{
+				break;
+			}
+			pq.pop();
+			pv_dest = currentPartition->getVertex(dest);
+			//explore the outgoing vertexes
+			//First, check if the destination is in the same connected component. If so, just search for the shortest path
+			pv = findVertex(currVId, labels);
+			if(pv != NULL && currentPartition->)
+			fanOut = pv->getVertex()->fanOut();
+			double edgeCost = 1;
+			for(int i = 0; i < fanOut; i++)
+			{
+				pe = pv->getOutEdges()->at(i);
+				if(!std::find(labels->begin(), labels->end(), pe->getLabel())) continue;
+				candVertexId = pe->getTo()->getID();
+
+				if (spColumnIndexInEdgesTable >= 0)
+				{
+					edgeTuple = this->getEdgeTuple(e->getTupleData());
+					edgeCost = ValuePeeker::peekDouble(edgeTuple->getNValue(spColumnIndexInEdgesTable));
+				}
 
 
+				//these lines are commented to allow top k search
+				//if ( (costMap.find(candVertexId) == costMap.end()) ||
+				//	 (costMap[candVertexId] > minCost + 1) )
+				{
+					//costMap[candVertexId] = minCost + 1;
+					pq.push(make_pair(minCost + edgeCost, make_pair(candVertexId, length+1)));
+				}
+
+			}
+		}
+
+		std::stringstream paramsToPrint;
+		paramsToPrint << "TopK SP: from = " << src << ", to = " << dest
+				<< ", k = " << k
+				<< ", foundPaths = " << foundPathsSoFar
+				<< ", numOfRowsAdded = " << m_pathTable->activeTupleCount();
+		LogManager::GLog("GraphView", "TopK_SP", 334, paramsToPrint.str());
 	}
 
 }
--- src/ee/graph/EDPIndex.h
+++ src/ee/graph/EDPIndex.h
@@ -12,18 +12,19 @@ class PartitionVertex;
 class EDPIndex;
 
 class PartitionEdge {
+	friend class Partition;
 public:
-	PartitionEdge(Edge* edge, PartitionVertex* fromVertex, PartitionVertex* toVertex, int weightField, int labelField, bool directed);
+	PartitionEdge(Edge* edge, PartitionVertex* fromVertex, PartitionVertex* toVertex, int weightField, int labelField);
 	~PartitionEdge(void);
 
-	Edge* getEdge();
+	Edge* getEdge() { return edge; }
 
-	int getID();
-	PartitionVertex* getFrom();
-	PartitionVertex* getTo();
+	int getID() { return eID; }
+	PartitionVertex* getFrom() { return fromV; }
+	PartitionVertex* getTo() { return toV; }
 
-	double getWeight();
-	int getLabel();
+	double getWeight() { return weight; }
+	int getLabel() { return label; }
 
 private:
 	Edge* edge;
@@ -39,39 +40,49 @@ private:
 class Partition;
 
 class PartitionVertex {
+	friend class Partition;
 public:
 	PartitionVertex(Vertex * vertex);
 	~PartitionVertex(void);
 
-	Vertex* getVertex();
+	Vertex* getVertex() { return vertex; }
 
-	int getID();
-	bool isBridge();
-	int getComponentID();
-	Partition* getPartition();
+	int getID() { return vID; }
+	bool isBridge() { return bridge; }
+	bool isBridgeBackwards() { return bridgeBackwards; }
+	int getComponentID() { return componentId; }
+	Partition* getPartition() { return partition; }
 
-	map<int, PartitionEdge*>* getInEdges();
-	map<int, PartitionEdge*>* getOutEdges();
+	map<int, PartitionEdge*>* getInEdges() { return inEdges; }
+	map<int, PartitionEdge*>* getOutEdges() { return outEdges; }
 
-	vector<Partition*>* getOtherHosts();
+	map<int, Partition*>* getOtherHostsOut() { return otherHosts_out; }
+	map<int, Partition*>* getOtherHostsIn() { return otherHosts_in; }
 
 	void setPartition(Partition * p) { partition = p; }
-	//Adds an edge to this vertex and updates OtherHostsList if applicable. Partition must be set first
-	void addEdge(PartitionEdge* edge, EDPIndex* edp);
+	void setBridge(bool b) { bridge = b; }
+	void setBridgeBackwards(bool b) { bridgeBackwards = b; }
+	void setComponentId(int cId) { componentId = cId; }
+	//Adds an edge to this vertex
+	void addEdge(PartitionEdge* edge, bool outEdge);
+
+
 
 private:
 	Vertex* vertex;
 
 	int vID;
 	bool bridge;
-	int componentID;
+	bool bridgeBackwards;
+	int componentId;
 
 	Partition* partition;
 
 	map<int, PartitionEdge*> inEdges;
 	map<int, PartitionEdge*> outEdges;
 
-	map<int, Partition*> otherHosts;
+	map<int, Partition*> otherHosts_out;
+	map<int, Partition*> otherHosts_in;
 };
 
 class MonochromePath{
@@ -87,45 +98,132 @@ protected:
 };
 
 class PathCacheEntry {
+	friend class Partition;
 public:
-	PathCacheEntry(int src, int dest, int cost, vector<PartitionEdge*>* pathEdges);
+	PathCacheEntry(int label, int src, int dest, int cost, vector<PartitionEdge*>* pathEdges);
 	~PathCacheEntry();
 
-	int getSource();
-	int getDestination();
-	int getCost();
+	int getLabel() { return label; }
+	int getSource() { return src; }
+	int getDestination() {return dest; }
+	int getCost() { return cost; }
 
-	vector<PartitionEdge*> getPath();
+	vector<PartitionEdge*>* getPath() { return &pathEdges; }
 
 	//Returns the size of the cache entry in bytes
-	int getEntrySize();
+	uint64_t getEntrySize();
+
+	int getLRU() { return lru_access; }
+	void setLRU(int lru) { lru_access = lru; }
 
 private:
+	int label;
 	int src;
 	int dest;
 	int cost;
 
 	vector<PartitionEdge*> pathEdges;
+
+	int lru_access;
+	int timestamp;
 };
 
-class Partition{
-
+class PathCacheComparator {
+	friend class PathCacheEntry;
 public:
-	Partition(int label);
+	PathCacheComparator();
+	bool operator() (PathCacheEntry& lhs, PathCacheEntry&rhs) const
+	  {
+	    return lhs.lru_access > rhs.lru_access;
+	  }
+};
+
+typedef std::priority_queue<PathCacheEntry*,std::vector<PathCacheEntry*>,PathCacheComparator> pathcachepq_type;
+
+//Data structure to track last-updated-timestamps of each SCC in a partition
+class PartitionConnectedComponents{
+public:
+	PartitionConnectedComponents(int initialNumOfComponents, Partition* partition);
+	void addConnectedComponent(int componentId);
+	void removeConnectedComponent(int componentId);
+	void advanceComponentTimestamp(int componentId);
+	//Merge component2 into component1
+	void mergeConnectedComponents(int componentId1, int componentId2);
+
+	int getLastTimestamp() { return lastTimestamp; }
+	int advanceTimestamp() { return ++lastTimestamp; }
+
+	bool inSameComponent(PartitionVertex* v1, PartitionVertex* v2) { return v1->getComponentID() != -1 && (v1->getComponentID() == v2->getComponentID()); }
+
+	int getConnectedComponentCount() { return numOfConnectedComponents; }
+
+private:
+	Partition* partition;
+	map<int, int> componentLastUpdateTimestamp;
+
+	int lastTimestamp = -1;
+	int numOfConnectedComponents = 0;
+};
+
+class DistFromSource{
+	friend class DistFromSourceComparator;
+	friend class Partition;
+public:
+	DistFromSource(int vId, double dist);
+private:
+	int vertexId;
+	double distance;
+};
+
+class DistFromSourceComparator {
+	friend class PathCacheEntry;
+public:
+	DistFromSourceComparator();
+	bool operator() (DistFromSource& lhs, DistFromSource&rhs) const
+	  {
+	    return lhs.distance > rhs.distance;
+	  }
+};
+
+typedef std::priority_queue<DistFromSource*,std::vector<DistFromSource*>,DistFromSourceComparator> distfromsourcepq_type;
+
+class Partition{
+friend class PartitionConnectedComponents;
+public:
+	Partition(EDPIndex* index, int label);
 	~Partition();
 
-	int getLabel();
+	int getLabel() { return label; }
 
-	map<int, PartitionVertex>* getVertexes();
-	map<int, PartitionEdge>* getEdges();
+	map<int, PartitionVertex>* getVertexes() { return p_vertexes; }
+	map<int, PartitionEdge>* getEdges() { return p_edges; }
+
+	PartitionVertex* getVertex(int vId);
+	PartitionEdge* getEdge(int eId);
 
 	PartitionVertex* addVertex(int id);
-	PartitionEdge* addEdge(int id, int from, int to, float weight, int label, bool addVertexIfNotFound);
+	PartitionEdge* addEdge(int id, int from, int to, int weightField, int labelField);
+
+	PartitionConnectedComponents* getConnectedComponents() { return &connectedComponents; }
+	float getEdgeWeight(int from, int to);
+
 private:
+	EDPIndex* index;
 	int label;
 	//Map of VertexID to PartitionVertex object in this Partition
-	map<int, PartitionVertex > p_vertexes;
-	map<int, PartitionEdge > p_edges;
+	map<int, PartitionVertex* > p_vertexes;
+	map<int, PartitionEdge* > p_edges;
+
+	//Index of PartitionVertex by ConnectedComponentId
+	map<int, vector<PartitionVertex*>> cc_vertexes;
+
+	//Strongly Connected Components calculation
+	PartitionConnectedComponents connectedComponents;
+	int numOfComponents;
+	void buildCC();
+	void CC_dfs(PartitionVertex* startVertex, vector<bool>& isVisited, map<int, int>* gToLId, int& curCompId);
+	float dijkstra(PartitionVertex* startVertex, PartitionVertex* endVertex);
+
 };
 
 class EDPIndex{
@@ -133,25 +231,49 @@ class EDPIndex{
 	friend class Partition;
 
 public:
-	EDPIndex();
-	static EDPIndex buildIndex(GraphView* g, int numLabels, int labelColumn, uint32_t cacheSize);
+	typedef pair<double, pair<int, int> > PQEntryWithLength;
+	typedef pair<int, int > PQEntry;
 
-	void addVertex(Vertex * v);
-	void addEdge(Edge * e);
+	EDPIndex();
+	~EDPIndex();
+	static EDPIndex buildIndex(GraphView* g, int numLabels, int labelColumn, int costColumn, uint32_t cacheSize);
+
+	PartitionVertex* addVertex(Partition * p, Vertex * v);
+	PartitionEdge* addEdge(Edge * e);
 
 	Partition* getPartition(int label);
+
+	PartitionVertex* findVertex(int v, vector<int>* labels);
+
+	//Queries
+	void SP_TopK(int src, int dest, int k, vector<int>* labels);
+
 
 protected:
 
 private:
 	GraphView* graph;
-	Partition* partitions;
+	vector<Partition*> partitions;
 
 	//Direct references from vertex ID to vertex, outside of partitions
 	map<int, PartitionVertex*> i_vertexes;
 
-	uint64_t cacheSize;
+	//Column index of label for edges
+	int labelColumn;
+	//Column index of cost for edges
+	int costColumn;
 
+	//Cache fields
+	uint64_t maxCacheSize;
+	uint64_t cacheSize;
+	//Cache. Index of vector is partition label, second key is source node, third key is destination node
+	uint64_t lru_clock;
+	vector<map<int, map<int, PathCacheEntry>>> cache;
+	pathcachepq_type lru;
+
+	//Adds a cache entry, replacing using LRU if necessary
+	void insertPath(int label, int src, int dest, double cost, vector<PartitionEdge*>* pathEdges);
+	PathCacheEntry* getPath(int label, int src, int dest);
 };
 
 }
